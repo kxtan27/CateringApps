139604c01b47c01c6f6c69dfc5e0fd5043ca1980
kxtan27 <tan.kaixiang27@gmail.com>
2020-04-17 01:35:25 +0800

    can change/reset password, sign out

--- app/src/main/java/com/example/cateringapps/ProfileActivity.kt
+++ app/src/main/java/com/example/cateringapps/ProfileActivity.kt
@@ -2,17 +2,39 @@ package com.example.cateringapps
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity:AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
+        auth = FirebaseAuth.getInstance()
+
         button_changepw.setOnClickListener{
             startActivity(Intent(this, ResetPasswordActivity::class.java))
         }
+
+        button_sign_out.setOnClickListener{
+            Toast.makeText(this, "Sign Out", Toast.LENGTH_SHORT).show()
+            signout()
+        }
+
+        auth.addAuthStateListener {
+            if (auth.currentUser == null){
+                this.finish()
+            }
+        }
+    }
+
+    private fun signout(){
+        FirebaseAuth.getInstance().signOut()
+        startActivity(Intent(this, MainActivity::class.java))
     }
 }
