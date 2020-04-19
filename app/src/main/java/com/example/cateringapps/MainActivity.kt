6379571f77ca6d915921a15cde1c37a68208091e
kxtan27 <tan.kaixiang27@gmail.com>
2020-04-20 00:05:14 +0800

    0.6

--- app/src/main/java/com/example/cateringapps/MainActivity.kt
+++ app/src/main/java/com/example/cateringapps/MainActivity.kt
@@ -1,17 +1,13 @@
 package com.example.cateringapps
 
-import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Patterns
-import android.widget.EditText
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import kotlinx.android.synthetic.main.activity_login.*
-import kotlinx.android.synthetic.main.activity_main.sign_up_page_button
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,72 +16,16 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main)
         auth = FirebaseAuth.getInstance()
 
         sign_up_page_button.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
         }
 
-        sign_in_button.setOnClickListener {
-            doLogin()
+        sign_in_page_button.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
         }
-
-        forget_pw_button.setOnClickListener {
-            val builder = AlertDialog.Builder(this)
-            builder.setTitle("Enter your email.")
-            val view = layoutInflater.inflate(R.layout.dialog_forgetpw, null)
-            val email = view.findViewById<EditText>(R.id.email)
-            builder.setView(view)
-            builder.setPositiveButton("Reset", DialogInterface.OnClickListener { _, _ ->
-                forgotpassword(email)
-            })
-            builder.setNegativeButton("Close", DialogInterface.OnClickListener { _, _ -> })
-            builder.show()
-        }
-    }
-
-    private fun forgotpassword(email: EditText) {
-        if (email.text.toString().isEmpty()) {
-            return
-        }
-        if (!Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches()) {
-            return
-        }
-
-        auth.sendPasswordResetEmail(email.text.toString())
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    Toast.makeText(this, "Email sent.", Toast.LENGTH_SHORT).show()
-                }
-            }
-    }
-
-    private fun doLogin() {
-        if (username.text.toString().isEmpty()) {
-            username.error = "Please Enter Email!!!"
-            username.requestFocus()
-        }
-
-        if (!Patterns.EMAIL_ADDRESS.matcher(username.text.toString()).matches()) {
-            username.error = "Please Enter Email!!!"
-            username.requestFocus()
-        }
-
-        if (password.text.toString().isEmpty()) {
-            password.error = "Please Enter Password!!!"
-            password.requestFocus()
-        }
-
-        auth.signInWithEmailAndPassword(username.text.toString(), password.text.toString())
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    val user = auth.currentUser
-                    updateUI(user)
-                } else {
-                    updateUI(null)
-                }
-            }
     }
 
     public override fun onStart() {
@@ -99,11 +39,6 @@ class MainActivity : AppCompatActivity() {
             if (currentUser.isEmailVerified) {
                 startActivity(Intent(this, FragmentGallery::class.java))
             }
-        } else {
-            Toast.makeText(
-                baseContext, "Please verify your email address",
-                Toast.LENGTH_SHORT
-            ).show()
         }
     }
 }
